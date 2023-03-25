2adbc1c11e7c0dceee31936c130bf3b39c4c1c8e
ZijunBAILAN <zk74@drexel.edu>
2023-03-25 18:35:24 +0800

    Add the sound effects.

--- js/index.js
+++ js/index.js
@@ -1,51 +1,63 @@
 const zodiacData = {
-    "Capricorn": {
+    "Capricornus": {
         "date": "Dec. 21 — Jan. 21",
-        "description": "Capricorn in Love is earthy and status-minded."
+        "description": "Capricorn in Love is earthy and status-minded.",
+        "sound": "sounds/Capricornus.wav"
     },
     "Sagittarius": {
         "date": "Nov. 21 — Dec. 21",
-        "description": "Sagittarius in Love is philosophical and feisty."
+        "description": "Sagittarius in Love is philosophical and feisty.",
+        "sound": "sounds/Sagittarius.wav"
     },
     "Scorpio": {
         "date": "Oct. 21 — Nov. 21",
-        "description": "Scorpio in Love is secretive and intense."
+        "description": "Scorpio in Love is secretive and intense.",
+        "sound": "sounds/Scorpio.wav"
     },
     "Libra": {
         "date": "Sept. 21 — Oct. 21",
-        "description": "Libra in Love is a natural sounding board, with striking clarity."
+        "description": "Libra in Love is a natural sounding board, with striking clarity.",
+        "sound": "sounds/Libra.wav"
     },
     "Virgo": {
         "date": "Aug. 21 — Sept. 21",
-        "description": "Virgo in Love is sincere and discriminating."
+        "description": "Virgo in Love is sincere and discriminating.",
+        "sound": "sounds/Virgo.wav"
     },
     "Leo": {
         "date": "July 21 — Aug. 21",
-        "description": "Leo in Love is regal and proud."
+        "description": "Leo in Love is regal and proud.",
+        "sound": "sounds/Leo.wav"
     },
     "Cancer": {
         "date": "June 21 — July 21",
-        "description": "Cancer in Love is self-protective and nurturing."
+        "description": "Cancer in Love is self-protective and nurturing.",
+        "sound": "sounds/Cancer.wav"
     },
     "Gemini": {
         "date": "May 21 — June 21",
-        "description": "Gemini in Love is playful and fun."
+        "description": "Gemini in Love is playful and fun.",
+        "sound": "sounds/Gemini.wav"
     },
     "Taurus": {
         "date": "April 21 — May 21",
-        "description": "Taurus in Love is sensual and loyal."
+        "description": "Taurus in Love is sensual and loyal.",
+        "sound": "sounds/Taurus.wav"
     },
     "Aries": {
         "date": "March 21 — April 21",
-        "description": "Aries in Love is a passionate fool."
+        "description": "Aries in Love is a passionate fool.",
+        "sound": "sounds/Aries.wav"
     },
     "Pisces": {
         "date": "Feb. 21 — March 21",
-        "description": "Pisces in Love is romantic and enchanting."
+        "description": "Pisces in Love is romantic and enchanting.",
+        "sound": "sounds/Pisces.mp3"
     },
     "Aquarius": {
         "date": "Jan. 21 — Feb. 21",
-        "description": "Aquarius in Love is unconventional."
+        "description": "Aquarius in Love is unconventional.",
+        "sound": "sounds/Aquarius.mp3"
     }
 }
 
@@ -90,4 +102,15 @@ function show_zodiac_detail(zodiac){
     document.querySelector("#name").innerHTML = "Name: " + zodiac;
     document.querySelector("#date").innerHTML = "Date: " + zodiacData[zodiac]['date'];
     document.querySelector("#description").innerHTML = "Description: " + zodiacData[zodiac]['description'];
+    // Play the sound effect
+    play_sound_effect(zodiacData[zodiac]["sound"]);
+}
+
+// Play sound effect
+function play_sound_effect(sound_name){
+    if(typeof(audio_element) != 'undefined'){
+        audio_element.pause();
+    }
+    audio_element = new Audio(sound_name);
+    audio_element.play();
 }
